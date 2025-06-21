8b58b7dbdea76224ee9443a0d465df58a4db1bb8
Hamad Khan <damahnahk@gmail.com>
2025-06-21 09:49:07 +0500

    updates

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from core import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
-    path('contact-us/', views.contact_us, name='contact_us'),
+    path('contact-us/', views.contact, name='contact'),
+    path('about-us/', views.about, name='about'),
 ]
