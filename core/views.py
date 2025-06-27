624381c89233b7d26b4ef723e49795c9b07ee370
Hamad Khan <damahnahk@gmail.com>
2025-06-27 20:16:01 +0500

    updates to homepage

--- core/views.py
+++ core/views.py
@@ -1,30 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category , Blog
+from datetime import datetime, timedelta
 
 # Create your views here.
 def home(request):
-    return render(request, 'core/home.html')
+  tech = Blog.objects.filter(category__name='Tech', status=Blog.PUBLISHED).order_by('-created_at')[:5]
+  politics = Blog.objects.filter(category__name='Politics', status=Blog.PUBLISHED).order_by('-created_at')[:5]
+  entertainment = Blog.objects.filter(category__name='Entertainment', status=Blog.PUBLISHED).order_by('-created_at')[:5]
+  finance = Blog.objects.filter(category__name='Finance', status=Blog.PUBLISHED).order_by('-created_at')[:5]
+  music = Blog.objects.filter(category__name='Music', status=Blog.PUBLISHED).order_by('-created_at')[:5]
+  life = Blog.objects.filter(category__name='Life', status=Blog.PUBLISHED).order_by('-created_at')[:5]
+
+  # Trending
+  now = datetime.now()
+  one_week_ago = now - timedelta(days=2)
+  trending_blogs = Blog.objects.filter(created_at__gte=one_week_ago, status=Blog.PUBLISHED).order_by('-view_count')[:5]
+
+  blogs = {'Trending': trending_blogs, 'Tech': tech, 'Politics': politics, 'Entertainment': entertainment, 'Finance': finance, 'Music': music, 'Life': life} 
+  return render(request, 'core/home.html', context={ 'blogs' : blogs })
 
 def contact(request):
-    return render(request, 'core/contact.html')
+  return render(request, 'core/contact.html')
 
 def about(request):
-    return render(request, 'core/about.html')
+  return render(request, 'core/about.html')
 
 
 # Categories Page
 def categories(request):
 
-    return render(request, 'core/categories.html')
+  return render(request, 'core/categories.html')
 
 def category_posts(request, slug):
-    category = get_object_or_404(Category, slug=slug)
-    blogs = Blog.objects.filter(category=category, status=Blog.PUBLISHED).order_by('-created_at')
-    return render(request, 'core/category_posts.html', context={'category': category, 'blogs': blogs})
+  category = get_object_or_404(Category, slug=slug)
+  blogs = Blog.objects.filter(category=category, status=Blog.PUBLISHED).order_by('-created_at')
+  return render(request, 'core/category_posts.html', context={'category': category, 'blogs': blogs})
 
 # Temp Urls
 def blog(request):
-    return render(request, 'core/blog.html')
+  return render(request, 'core/blog.html')
 
 def authors(request):
-    return render(request, 'core/authors.html')
+  return render(request, 'core/authors.html')
