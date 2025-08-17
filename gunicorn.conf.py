9f7a03a7ebebefb14695b7424ac4ba5dd11508b3
Hamad Khan <damahnahk@gmail.com>
2025-08-17 19:07:07 +0500

    updates

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,4 +3,3 @@ import os
 workers = int(os.getenv("GUNICORN_WORKERS", "1"))
 worker_class = os.getenv("GUNICORN_WORKER_CLASS", "uvicorn.workers.UvicornWorker")
 bind = os.getenv("GUNICORN_BIND", "0.0.0.0:8000")
-timeout = int(os.getenv("GUNICORN_TIMEOUT", "120"))
